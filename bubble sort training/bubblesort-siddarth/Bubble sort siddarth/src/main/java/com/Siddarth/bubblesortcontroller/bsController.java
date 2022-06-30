63b402850c3fe96af35402b231b26aa36b3c2315
Sidd7226 <107983293+Sidd7226@users.noreply.github.com>
2022-06-30 16:36:10 +0530

    Add files via upload

---
+++ bubble sort training/bubblesort-siddarth/Bubble sort siddarth/src/main/java/com/Siddarth/bubblesortcontroller/bsController.java
@@ -0,0 +1,26 @@
+package com.Siddarth.bubblesortcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Siddarth.bubblesortservice.bsService;
+
+@RestController
+public class bsController {
+
+	@Autowired
+	bsService bsservice;
+
+	@GetMapping("/bubblesort")
+	public int[] run(@RequestBody int[] array) {
+		bsservice.bubblesort(array);
+		return array;
+	}
+//	public int[] run(@RequestBody int[] array) {
+//	bsservice.bubblesort(array);
+//	return array;
+//	}    
+
+}
